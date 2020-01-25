a781ef2d3220f6a180ec199aea6be64d893f9196
ir2010 <46022116+ir2010@users.noreply.github.com>
2020-01-25 16:59:43 +0530

    Initial commit

---
+++ app/src/main/java/com/ir_sj/lightsensor/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ir_sj.lightsensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    MediaPlayer mp;
+    SensorManager sm;
+    Sensor s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.sl);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        s = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sm.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.values[0] > 2)
+            mp.start();
+        else
+            mp.pause();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+}
